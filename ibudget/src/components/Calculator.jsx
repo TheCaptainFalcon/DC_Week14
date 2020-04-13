2f5b95733a5e9fc5a2b4e9ab0bca85c335978c6e
Joseph Kim <josephkim570@gmail.com>
2020-04-13 12:33:01 -0400

    Added placeholder content for Home component and created a simple input form for the income and expenses sections for the Calculator component

--- ibudget/src/components/Calculator.jsx
+++ ibudget/src/components/Calculator.jsx
@@ -2,14 +2,86 @@ import React, { Component } from 'react';
 import './css/Calculator.css';
 
 class Calculator extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
+    state = {
+        form: [
+            { }
+        ]
     }
+
     render() { 
         return (  
-            <div>
-                <p>This is the calculation page ha</p>
+            <div className='Calculator'>
+                <h2>Income</h2>
+                <form className='Calculator-form'>
+                    <label>Name
+                        <input
+                        type='text'
+                        name={this.state.name}
+                        placeholder='Name'
+                        />
+                    </label>
+                    <label>Description
+                        <input
+                        type='text'
+                        name={this.state.name}
+                        placeholder='Description'
+                        />
+                    </label>
+                    <label>Frequency
+                        <select>
+                            <option value="Daily">Daily</option>
+                            <option value="Weekly">Weekly</option>
+                            <option value="Bi-Weekly">Bi-Weekly</option>
+                            <option value="Monthly">Monthly</option>
+                            <option value="Annually">Annually</option>
+                        </select>
+                    </label>
+                    <label>Amount
+                        <input
+                        type='number'
+                        name={this.state.name}
+                        placeholder='$'
+                        />
+                    </label>
+                    <button>+</button>
+                </form>
+
+                {/* Start of the Expenses */}
+
+                <h2>Expenses</h2>
+                <form className='Calculator-form'>
+                    <label>Name
+                        <input
+                        type='text'
+                        name={this.state.name}
+                        placeholder='Name'
+                        />
+                    </label>
+                    <label>Description
+                        <input
+                        type='text'
+                        name={this.state.name}
+                        placeholder='Description'
+                        />
+                    </label>
+                    <label>Frequency
+                        <select>
+                            <option value="Daily">Daily</option>
+                            <option value="Weekly">Weekly</option>
+                            <option value="Bi-Weekly">Bi-Weekly</option>
+                            <option value="Monthly">Monthly</option>
+                            <option value="Annually">Annually</option>
+                        </select>
+                    </label>
+                    <label>Amount
+                        <input
+                        type='number'
+                        name={this.state.name}
+                        placeholder='$'
+                        />
+                    </label>
+                    <button>+</button>
+                </form>
             </div>
         );
     }
--- ibudget/src/components/Home.jsx
+++ ibudget/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './css/Home.css';
+import stonks from './image/stonks.png'
 
 class Home extends Component {
     state = {
@@ -8,8 +9,22 @@ class Home extends Component {
 
     render() {
         return(
-            <div>
-                <p>This is the home page</p>
+            <div className='Home'>
+                <h1>iBudget</h1>
+                <h2>A simple solution for calculating your income to expense ratios</h2>
+                <img src={stonks}/>
+                    <div className='Home-sections'>
+                        <h1>Description:</h1>
+                            <p>iBudget is capable of taking into account income and expenses of different amounts and variable frequencies to determine
+                                the results. Sometimes it is best to see the actual figures, rather than 'guess-timating' the amount.
+                            </p>
+                    </div>
+                    <div>
+                        <h1>Tutorial - How to get started</h1>
+                        <h2>Step 1:</h2>
+                        <p>Click on the Calculator tab</p>
+                    </div>
+                
             </div>
         )
     }
