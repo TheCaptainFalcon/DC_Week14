c7109185fb1bec5617b296251846fcba99238765
Joseph Kim <josephkim570@gmail.com>
2020-04-16 13:58:51 -0400

    Added app icon and changed the name - implemented bootstrap nav as a wrapper around react navlink to still use LinkTo instead of href

--- ibudget/src/App.js
+++ ibudget/src/App.js
@@ -3,15 +3,27 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 import Home from './components/Home';
 import Calculator from './components/Calculator';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav} from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <nav className='App-nav'>
-          <NavLink activeClassName='active-link' exact={true} to='/'>Home </NavLink>
-          <NavLink activeClassName='active-link' to='/calculator'>Calculator</NavLink>
-        </nav>
+        <Navbar className='App-nav' bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            <Nav.Link><NavLink activeClassName='active-link' exact={true} to='/'>Home</NavLink></Nav.Link>
+            <Nav.Link> <NavLink activeClassName='active-link' to='/calculator'>Calculator</NavLink></Nav.Link>
+          </Navbar.Brand>
+        </Navbar>
         <Switch>
           <Route exact path='/' component={ Home } />
           <Route exact path ='/calculator' component={ Calculator } />
--- ibudget/src/components/Home.jsx
+++ ibudget/src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './css/Home.css';
 import stonks from './image/stonks.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, CardDeck} from 'react-bootstrap';
 
 class Home extends Component {
     state = {
@@ -15,14 +17,16 @@ class Home extends Component {
                 <img src={stonks} alt='stonks.png'/>
                     <div className='Home-sections'>
                         <h1>Description:</h1>
-                            <p>iBudget is capable of taking into account income and expenses of different amounts and variable frequencies to determine
-                                the results. Sometimes it is best to see the actual figures, rather than 'guess-timating' the amount.
+                            <p>iBudget is capable of utilizing user-inputted data on monthly income and expenses to calculate the monthly balance.
+                                This core information is important to feed the calculation data for other time intervals that are of interest.
+                                Sometimes it is best to see the actual figures, rather than 'guess-timating' the amount.
                             </p>
                     </div>
                     <div>
-                        <h1>Tutorial - How to get started</h1>
-                        <h2>Step 1:</h2>
-                        <p>Click on the Calculator tab</p>
+                        <h1>Demo</h1>
+                        <p>To get started, click on the "Calculator" tab on the above navbar and insert the data fields.
+
+                        </p>
                     </div>
                 
             </div>
